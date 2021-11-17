2359cc59be55b594aa916cce39ed08978fc26685
leon.skinner@hotmail.co.uk <leon.skinner@bt.com>
2021-11-17 15:53:04 +0000

    Made it mostly wide screen freindly, make links do somthing, changed about us to what we do

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from re import DEBUG
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return render_template('index.html')
+
+@app.route("/Expiriance")
+def Expiriance():
+    return redirect('/')
+
+@app.route("/Services")
+def Services():
+    return redirect('/')
+
+@app.route("/Contact_Us")
+def Contact_Us():
+    return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
